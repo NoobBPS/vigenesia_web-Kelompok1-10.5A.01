85411c9bb69ae57d33fadf4859c981485595da62
Gray0Boy <86937285+NoobBPS@users.noreply.github.com>
2024-11-28 21:36:58 +0700

    Update PUTprofile.php

--- application/controllers/api/PUTprofile.php
+++ application/controllers/api/PUTprofile.php
@@ -25,50 +25,42 @@ class PUTprofile extends REST_Controller
 
     function index_put()
     {
-        $id = $this->put('iduser');
-
-        // Get the post data
-        $nama = strip_tags($this->put('nama'));
-
-        $profesi = strip_tags($this->put('profesi'));
-        $email = strip_tags($this->put('email'));
-        $password = $this->put('password');
-
-
-        // Validate the post data
-        if (!empty($nama)  && !empty($profesi) && !empty($email) && !empty($password)) {
-            // Update user's account data
-            $userData = array();
-            if (!empty($nama)) {
-                $userData['nama'] = $nama;
-            }
-
-            if (!empty($profesi)) {
-                $userData['profesi'] = $profesi;
-            }
-            if (!empty($email)) {
-                $userData['email'] = $email;
-            }
-            if (!empty($password)) {
-                $userData['password'] = md5($password);
-            }
-
-            $update = $this->user->update($userData, $id);
-
-            // Check if the user data is updated
-            if ($update) {
-                // Set the response and exit
-                $this->response([
-                    'status' => TRUE,
-                    'message' => 'user berhasil updated profile baru.'
-                ], REST_Controller::HTTP_OK);
-            } else {
-                // Set the response and exit
-                $this->response("Some problems occurred, please try again.", REST_Controller::HTTP_BAD_REQUEST);
-            }
+        $id = $this->put('id');
+        $data = array(
+            'id'           => $this->post('id'),
+            'nama'          => $this->post('nama'),
+            'instansi'    => $this->post('instansi'),
+            'tnggal_lahir'    => $this->post('tnggal_lahir'),
+            'umur'    => $this->post('umur'),
+            'jenis_kelamin'    => $this->post('jenis_kelamin'),
+            'alamat'    => $this->post('alamat'),
+            'email'    => $this->post('email'),
+            'call_num'    => $this->post('call_num'),
+            'image'    => $this->post('image'),
+            'password'    => $this->post('password'),
+            'role_id'    => $this->post('role_id'),
+            'is_active'    => $this->post('is_active'),
+            'tanggal_input'    => $this->post('tanggal_input')
+        );
+        $this->db->where('id', $id);
+        $update = $this->db->update('user', $data);
+        if ($update) {
+            $this->response($data, 200);
         } else {
-            // Set the response and exit
-            $this->response("Provide at least one user info to update.", REST_Controller::HTTP_BAD_REQUEST);
+            $this->response(array('status' => 'fail', 502));
         }
     }
+
+    function index_get()
+    {
+        $id = $this->get('id');
+        if ($id == '') {
+            $api = $this->db->get('user')->result();
+        } else {
+            $this->db->where('id', $id);
+            $api = $this->db->get('user')->result();
+        }
+        $this->response($api, 200);
+    }
+
 }
